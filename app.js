1e01fd72dd58d46995c5edcc2f4788271ccfd375
raphasilvac <raphasilvac@gmail.com>
2013-03-27 19:00:42 -0300

    Fixed database model

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , mongoq = require('mongoq');
+  , mongoq = require('mongoq')
+  , BSON = mongoq.BSON
+  , ObjectID = BSON.ObjectID;
 
 // Configurando a execução do banco MongoDB
 var   DB = 'db_test'
@@ -36,26 +38,48 @@ app.configure('production', function(){
 // Routes
 app.get('/', routes.index);
 
+// Index
 app.get('/users', routes.users);
 
+// Add
 app.get('/users/add', routes.users.add);
 
-app.get('/users/edit/:id?', function(req, res){
+// Show
+app.get('/users/show/:id?', function(req, res){
+  var id = req.params.id;
   res.render('users/edit', { title: '4fluxo' })
 });
 
-// Rota GET para listar os usuários
+// Edit
+app.get('/users/edit/:id', function(req, res){
+  var id = req.params.id;
+  res.render('users/edit', { title: '4fluxo' })
+});
+
+// JSON Routes
+// Get All
 app.get('/users.json', function(req, res) {
-   users.find().toArray(function(err, result){
+  users.find().toArray(function(err, result){
+    res.send(result);
+  });
+});
+
+// Get
+app.get('/users/:id.json', function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  users.findOne({
+    '_id': ObjectID(id)
+  }, function(err, result){
       res.send(result);
-   });
+  });
+
 });
 
 // Users Posts
 app.post('/users', function(req, res) {
    var user = req.body.user;
-   console.log(user);
-   users.insert({user : user});
+   users.insert(user);
    res.send(true);
 });
 
